adce3461dbe29b665ea1acc0b034d21d18a757c3
Lorna Harris <lharris11@ewu.edu>
2025-03-13 22:20:36 -0700

    Beginning of Game
    This is just the beginning of the game!

---
+++ scripts/objRefTable.js
@@ -0,0 +1,38 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Behaviors.Platform,
+		C3.Behaviors.bound,
+		C3.Behaviors.scrollto,
+		C3.Plugins.Tilemap,
+		C3.Plugins.TiledBg,
+		C3.Behaviors.jumpthru,
+		C3.Behaviors.solid,
+		C3.Plugins.Keyboard,
+		C3.Plugins.Keyboard.Cnds.OnKey,
+		C3.Plugins.Sprite.Acts.SetMirrored
+	];
+};
+self.C3_JsPropNameTable = [
+	{Platform: 0},
+	{BoundToLayout: 0},
+	{ScrollTo: 0},
+	{Marshall: 0},
+	{Wall: 0},
+	{Jumpthru: 0},
+	{TVStand: 0},
+	{Solid: 0},
+	{TiledBackground: 0},
+	{TV: 0},
+	{Keyboard: 0}
+];
+
+self.InstanceType = {
+	Marshall: class extends self.ISpriteInstance {},
+	Wall: class extends self.ITilemapInstance {},
+	TVStand: class extends self.ITiledBackgroundInstance {},
+	TiledBackground: class extends self.ITiledBackgroundInstance {},
+	TV: class extends self.ISpriteInstance {},
+	Keyboard: class extends self.IInstance {}
+}
